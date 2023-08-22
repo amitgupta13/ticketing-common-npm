92e2107e0e67843425826ea873eddb4ed394f073
abc <abc@abc.com>
2023-08-22 16:44:20 +0530

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// Re-export stuff from errors and middlewares
+// Re-export modules from errors and middlewares
 
 export * from "./errors/bad-request-error";
 export * from "./errors/custom-error";
